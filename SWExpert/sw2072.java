b4b9e2426ae8cdbb71d3253ea9e0213042fce7d1
cha <cdm2883@gmail.com>
2023-05-14 17:16:38 +0900

    SWExpertAcademy first commit

--- SWExpert/sw2072.java
+++ SWExpert/sw2072.java
@@ -3,13 +3,13 @@ import java.nio.Buffer;
 import java.util.*;
 
 public class sw2072 {
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         int testcase = sc.nextInt();
         int[] arr = new int[10];
         int sum = 0;
-
 
         for(int i = 0; i < testcase ; i++ ){
             for(int j = 0 ; j < 10 ; j++){
