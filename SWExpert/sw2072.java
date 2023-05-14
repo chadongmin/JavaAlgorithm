99901a016819ca19fc480028ac5e47de4da91700
cha <cdm2883@gmail.com>
2023-05-14 17:11:15 +0900

    1206. [S/W 문제해결 기본] 1일차 - View (D3)

---
+++ SWExpert/sw2072.java
@@ -0,0 +1,22 @@
+package SWExpert;
+import java.nio.Buffer;
+import java.util.*;
+
+public class sw2072 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int testcase = sc.nextInt();
+        int[] arr = new int[10];
+        int sum = 0;
+
+        for(int i = 0; i < testcase ; i++ ){
+            for(int j = 0 ; j < 10 ; j++){
+                arr[j] = sc.nextInt();
+                if(arr[j] % 2 != 0) sum+=arr[j];
+            }
+            System.out.println("#"+(i+1)+" "+ sum);
+            sum = 0;
+        }
+    }
+}
