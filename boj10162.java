b1d9be276c423798a006fed1a0dba3b73389fe2b
cha <cdm2883@gmail.com>
2023-05-21 20:57:49 +0900

    Greedy Algorithm

---
+++ boj10162.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class boj10162 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [] btn  ={300, 60, 10};
+        int cnt = 0;
+        int time = sc.nextInt();
+        int [] result = new int [3];
+
+            for(int i = 0;i<btn.length;i++) {
+                while (time >= btn[i]) {
+                    time-=btn[i];
+                    result[i]++;
+                }
+            }
+            if (time == 0){
+                for(var r : result){
+                    System.out.print(r+" ");
+                }
+            }
+            else System.out.println("-1");
+
+
+
+    }
+}
