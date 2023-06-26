87a8cf1ea9af5b5fb10f7bea72e4d26e01dd6db7
cha <cdm2883@naver.com>
2023-06-26 14:48:47 +0900

    .

---
+++ BOJ/DataStructure/boj1935.java
@@ -0,0 +1,16 @@
+package BOJ.DataStructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+/*
+* 아이디어 :
+* 테스트케이스 입력 뒤에 문자열 수식이 주어진다. A는 1, B는 2에 대응되는게 힌트인듯. (아스키코드에서 활용하면 매핑이 됨)
+* 문자열 수식을 중위표현식으로 바꿔야 한다.
+* 스택에 일단 다 넣어놓고, peek값이 피연산자면
+*
+* */
+public class boj1935 {
+
+}
