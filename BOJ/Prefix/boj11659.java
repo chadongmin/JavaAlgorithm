c434fb236b77fd8525c3c4bcbf2ce93c9a7249c4
cha <cdm2883@naver.com>
2023-07-04 19:36:05 +0900

    Prefix

---
+++ BOJ/Prefix/boj11659.java
@@ -0,0 +1,23 @@
+package BOJ.Prefix;
+
+import java.util.Scanner;
+
+public class boj11659 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        int loop = sc.nextInt();
+        int[] arr = new int[tc+1];
+        arr[0] = 0;
+
+        for(int i = 1 ; i<tc+1;i++){
+            arr[i] = arr[i-1] +  sc.nextInt();
+        }
+
+        for(int i = 0;i<loop;i++){
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+            System.out.println(arr[end] - arr[start-1]);
+        }
+    }
+}
